a0650b4f137f52f837fed491108b1e75e50dfc99
Samarash Eugene <samarasha95@gmail.com>
2025-06-01 18:44:30 +0300

    Pages added

---
+++ remember/src/pages/Faq/Faq.jsx
@@ -0,0 +1,9 @@
+const FaqPage = () => {
+    return (
+        <section>
+            <h1>F and Q</h1>
+        </section>
+    )
+}
+
+export default FaqPage
---
+++ remember/src/pages/Memories/Memories.jsx
@@ -0,0 +1,9 @@
+const Memories = () => {
+    return (
+        <section>
+            <h1>Memories</h1>
+        </section>
+    )
+}
+
+export default Memories
